b4e742c201200c1daac8c26eab21a07c40548b09
Thomas S Ledbetter Jr <tledbet94@gmail.com>
2024-12-08 10:38:47 -0500

    added back missing save_load.py

---
+++ layout/elements/control_panel/modes/save_load.py
@@ -0,0 +1,55 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+save_load_title = html.H1('Save & Load', className='edit-header-text')
+
+# Download section
+local_save_text = html.Div(html.P(children="Download Tree", className="save-load-text"),
+                           style={'textAlign': 'center'})
+
+# author, title
+
+local_save_input = dbc.Input(id='local-save-input', placeholder='Enter title', className='edit-input')
+
+author_input = dbc.Input(id='author-input', placeholder='Enter author', className='edit-input')
+
+description_input = dcc.Textarea(id='description-input', placeholder='Enter description', className='edit-input',
+                                 style={'height': '15vh', 'width': '100%'})
+
+local_save_button = dbc.Button(children=html.I(className="fa-solid fa-download icon-style-bottom-row"),
+                               id='local-save-button', className='save-load-button')
+
+local_save_dummy = dcc.Store(id='local_save_dummy')
+
+local_download = dcc.Download(id='local-download')
+
+# load
+local_load_text = html.Div(html.P(children="Upload Tree", className="save-load-text"),
+                           style={'textAlign': 'center'})
+
+upload_area = html.Div(dcc.Upload(
+    id='upload-area',
+    children=html.P('Drag & Drop || Click to Select File'),
+    multiple=False,  # Set to True if you want to allow multiple files
+    className='upload-area'
+), style={'text-align': 'center'})
+
+save_load = html.Div(
+    [
+        local_save_dummy,
+        local_download,
+        dcc.Store('upload-store'),
+        dcc.Store('upload-active', data=True),
+        save_load_title,
+        html.Div(style={'height': '0vh'}),
+        local_save_text,
+        dbc.Row([dbc.Col(local_save_input, align='center', width=8), dbc.Col(local_save_button, width=4)]),
+        html.Div(style={'height': '2vh'}),
+        dbc.Row(dbc.Col(author_input)),
+        html.Div(style={'height': '2vh'}),
+        dbc.Row(dbc.Col(description_input)),
+        html.Div(style={'height': '3vh'}),
+        local_load_text,
+        dbc.Row(dbc.Col(upload_area))
+    ]
+)
